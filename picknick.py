7112264234cd6adbdeec4fa5da0e1a6962a53037
innerlite <19549820+innerlite@users.noreply.github.com>
2019-02-27 14:41:43 +0100

    Update and rename NickLess.py to picknick.py

--- picknick.py
+++ picknick.py
@@ -2,19 +2,25 @@
 
 import weechat as w
 
-SCRIPT_NAME    = "NickrBot"
+SCRIPT_NAME    = "PickNick"
 SCRIPT_AUTHOR  = "innerlite"
-SCRIPT_VERSION = "0.01"
+SCRIPT_VERSION = "0.02a"
 SCRIPT_LICENSE = "GPL3"
-SCRIPT_DESC    = "NickrBot, The short & effective Nick(regain)Bot for headless irc networks"
+SCRIPT_DESC    = "PickNick, The short & effective nick regainer for 'headless' irc networks"
 
 if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
               SCRIPT_DESC, '', ''):
     w.hook_signal("irc_server_connected", SCRIPT_NAME, "")
+	
+#zonder deze var werkt het script ook omdat de string in de conf leeg is, probleem is dat het op elke server/buffer de nick veranderd.
+#test = w.config_string(w.config_get("irc.server_default.addresses"))
 
-def timer_cb(data, signal, server_name):
+def timer_cb(server_name, signal):
+    w.prnt('', '==>\t%s' % (server_name))
     il = w.infolist_get("irc_server", "", server_name)
     sbuffer = w.buffer_search("irc", server_name + "#")
+    cur_nick = ''
+    forced_nick = ''
     if w.infolist_next(il):
         cur_nick = w.infolist_string(il, 'nick')
         nicks = w.infolist_string(il, 'nicks')
@@ -22,9 +28,9 @@ def timer_cb(data, signal, server_name):
 #        password = w.infolist_string(il, 'password')
     w.infolist_free(il)
     if (cur_nick != forced_nick):
-#        w.command(sbuffer, "/msg nickserv ghost %s %s" % (forced_nick, password))
         w.command(sbuffer, "/nick %s" % (forced_nick))
+#        w.command(sbuffer, "/msg nickserv ghost %s %s" % (forced_nick, password))
 #        w.command(sbuffer, "/msg nickserv identify %s" % (password))
     return w.WEECHAT_RC_OK
-	
-w.hook_timer(3*100000, 0, 0, 'timer_cb', '')
+
+w.hook_timer(1000, 0, 0, 'timer_cb', '')
