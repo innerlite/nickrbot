a884ecb004cb538cdd6ce73ffc0309fc4b1d654f
innerlite <19549820+innerlite@users.noreply.github.com>
2019-02-28 19:21:48 +0100

    Update picknick.py

--- picknick.py
+++ picknick.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # innerlite Amsterdam 2019, poort587@gmail.com
 # Picknick is an oldschool nick regainer, it will check every 2 minutes
-# to get the taken nick back on networks without NickServ.
+# to take back the configurated nickname 
+# in your weechat settings for networks without NickServ.
 
 import weechat as w
 
