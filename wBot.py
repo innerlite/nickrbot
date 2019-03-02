91ebc063cdc68f1c00d122dcecc101e53a53e092
innerlite <19549820+innerlite@users.noreply.github.com>
2019-03-02 20:12:46 +0100

    Added commands

--- wBot.py
+++ wBot.py
@@ -7,24 +7,40 @@ SCRIPT_DESC    = "Weechat Bot Commands"
 import weechat as w
 
 def wtrigger(data, buffer, args):
-    global kserver, kchannel, knick, mode
+    global kserver, kchannel, knick, mode, query
     if options["enabled"] == "on":
         try:
             null, srvmsg = args.split(" PRIVMSG ", 1)
         except:
             return w.WEECHAT_RC_OK
+
         try:
-            kchannel, query = srvmsg.split(" :{} ".format(options["hello_trigger"]), 1)
+            kchannel, query = srvmsg.split(" :{} ".format(options["op_trigger"]), 1)
+            mode = "op"
         except ValueError:
-            return w.WEECHAT_RC_OK
+            try:
+                kchannel, query = srvmsg.split(" :{} ".format(options["kick_trigger"]), 1)
+                mode = "kick"
+            except ValueError:
+                try:
+                    kchannel, query = srvmsg.split(" :{} ".format(options["ban_trigger"]), 1) 
+                    mode = "ban"
+                except ValueError:
+                    try:
+                        kchannel, query = srvmsg.split(" :{} ".format(options["voice_trigger"]), 1)
+                        mode = "voice"
+                    except ValueError:    
+
+                        return w.WEECHAT_RC_OK
 
         kserver = str(buffer.split(",", 1)[0])
         knick = w.info_get("irc_nick_from_host", args)
-        query = query.replace(" ", "%20")
+#        query = query.replace(" ", "%20")
+        query = query.replace(" ", "")
+#        w.hook_process('*', 30 * 1000, "wbuffer", "")
+        auto_cmd = " ".format(query)
 
-        w.hook_process('*', 30 * 1000, "wbuffer", "")
-
-#        w.command("","/wait 1m /python unload %s" % SCRIPT_NAME) <-- test
+        w.hook_process(auto_cmd, 30 * 1000, "wbuffer", "")
 
     return w.WEECHAT_RC_OK
 
@@ -33,20 +49,27 @@ def wbuffer(reaction, data, command, out, er):
     buffer = w.info_get("irc_buffer", rtnbuf)
     botnick = w.info_get("irc_nick", kserver)
     if kchannel == botnick:
-        #private
-        command = "msg {} {}".format(knick, reaction)
-    else:
-        #channel
-#        command = "msg {} {}".format(kchannel, reaction) <-- origineel
-        command = "msg {} hello".format(kchannel, reaction)
+        #in private
 
-#    w.prnt("", command) <-- test
+#        command = "msg {} {}".format(knick, reaction) origineel
+        if mode == 'op': command = "op {} {}".format(knick, query)
+        if mode == 'kick': command = "kick {} {}".format(knick, query)
+        if mode == 'ban': command = "ban {} {}".format(knick, query)
+        if mode == 'voice': command = "voice {} {}".format(knick, query)    
+
+    else:
+        #on channel
+
+            if mode == 'op': command = "op {} {}".format(kchannel, query)
+            if mode == 'kick': command = "kick {} {}".format(kchannel, query)
+            if mode == 'ban': command = "ban {} {}".format(kchannel, query)
+            if mode == 'voice': command = "voice {} {}".format(knick, query)       
 
     cmdprefix = "/"
     w.command(buffer, cmdprefix + command)
-
     return w.WEECHAT_RC_OK
-	
+
+
 ### begin weechat plugin configuration stuff ###
 def config_cb(data, option, value):
     """callback when script option is changed."""
@@ -54,14 +77,17 @@ def config_cb(data, option, value):
     options[opt] = value
     return w.WEECHAT_RC_OK
 
+
 def get_option(option):
     """returns value of w.option."""
     return w.config_string(w.config_get("{}.{}".format(plugin_config, option)))
 
-default_options = {"enabled": "on",
-                   "hello_trigger": "!hello"}
 
 plugin_config = "plugins.var.python.{}".format(SCRIPT_NAME)
+default_options = {"enabled": "on",
+                   "op_trigger": "!op",
+                   'kick_trigger': '!kick'}
+
 
 if __name__ == "__main__":
     if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
@@ -70,13 +96,25 @@ if __name__ == "__main__":
                 w.config_set_plugin(option, value)
 
         options = {"enabled": get_option("enabled"),
-                   "hello_trigger": get_option("hello_trigger")
+                   "op_trigger": get_option("op_trigger"),
+                   "kick_trigger": get_option("kick_trigger"),
+                   "ban_trigger": get_option("ban_trigger"),
+                   "voice_trigger": get_option("voice_trigger"),
                   }  
 ### end weechat plugin configuration stuff ####
 
-        # start
-        w.hook_signal("*,irc_in_privmsg", "wtrigger", "data")
-        w.hook_config("{}.*".format(plugin_config), "config_cb", "")
+# start
+w.hook_signal("*,irc_in_privmsg", "wtrigger", "data")
+w.hook_config("{}.*".format(plugin_config), "config_cb", "")
+
+
+
+
+
+
+
+
+
 
 
 
