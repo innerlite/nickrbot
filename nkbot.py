cdbdbf643abd23ba75a9e93045a058accefd43a3
innerlite <19549820+innerlite@users.noreply.github.com>
2019-03-05 01:27:28 +0100

    Update nkbot.py

--- nkbot.py
+++ nkbot.py
@@ -1,20 +1,29 @@
-import weechat as w
+import weechat
 
-w.register('hal', 'hal9000', '6.6.6', 'GPL3', 'HAL Script', '', '')
+weechat.register('hal', 'hal9000', '6.6.6', 'GPL3', 'HAL Script', '', '')
 
-users = [ '24-96-22-211.telfoort.nl', 'marktpflantz.de' ]
+users = [ '*!*@82-197-212-247.dsl.cambrium.nl', '*!*@ip565b9f6e.direct-adsl.nl' ]
 
 def priv_cb(data, signal, signal_data):
+
     args = signal_data.split(' ')[0:3]
     nick = args[0][1:].split('!')[0]
     user = args[0][1:].split('!')[1].split('@')[0]
     host = args[0][1:].split('!')[1].split('@')[1]
+    target = args[2]
     message = signal_data[1:]
-#   message = message[message.find(':') + 1:]
-    w.prnt('', 'HAL\t' + signal_data)
-#   w.prnt('', 'HAL\t' + nick + ' +++ ' + user + ' +++ ' + host)
-    if any(host in s for s in users):
-    w.command('','/' + message)
-    return w.WEECHAT_RC_OK
+    message = message[message.find(':') + 1:]
 
-w.hook_signal('*,irc_in2_privmsg', 'priv_cb', '')
+    # weechat.prnt('', 'HAL\t' + signal_data)
+    # weechat.prnt('', 'HAL\t' + nick + ' +++ ' + user + ' +++ ' + host + ' +++ ' + target)
+
+    if any(host in s for s in users):
+        if message[0] == '/':
+            weechat.command('', message)
+        # weechat.prnt('', 'HAL\tMatch!')
+        # weechat.prnt('', 'HAL\t' + message)
+    # 
+    return weechat.WEECHAT_RC_OK
+
+weechat.hook_signal('*,irc_in2_privmsg', 'priv_cb', '')
+
