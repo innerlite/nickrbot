d89edf0c1484684eddac629e98d0a1db3fb3114f
innerlite <19549820+innerlite@users.noreply.github.com>
2019-07-17 10:55:23 +0200

    Update chanlimiter.py

--- chanlimiter.py
+++ chanlimiter.py
@@ -8,15 +8,18 @@ import weechat
 
 weechat.register('rxtx', 'Channel limiter', '0.01', 'GPL3', 'Channel limiter', '', '')
 
-def cl_timer_cb(data, signal, signal_data, remaining_calls):
+def cl_timer_cb(data, remaining_calls):
+    weechat.command('', 'cl_cmd_cb', '')
+    return weechat.WEECHAT_RC_OK
 
+weechat.hook_timer(60000, 0, 0, 'cl_timer_cb', '')
+
+def cl_cmd_cb(data, signal, signal_data):
     server = signal.split(",")[0]
     msg = weechat.info_get_hashtable("irc_message_parse", {"message": signal_data})
     buffer = weechat.info_get("irc_buffer", "%s,%s" % (server, msg["channel"]))
     count = weechat.string_eval_expression("${buffer.nicklist_count}", {"buffer": buffer}, {}, {})
-
+    
     weechat.prnt(buffer, 'total users: ' +  count)
 
     return weechat.WEECHAT_RC_OK
-
-weechat.hook_timer(60000, 0, 0, 'cl_timer_cb', ' ')
