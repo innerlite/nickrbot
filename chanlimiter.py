1ef2bc80f905cce0c07ba613c1edb63baa3674c2
innerlite <19549820+innerlite@users.noreply.github.com>
2019-07-20 12:01:55 +0200

    Update chanlimiter.py
    limiter does not change the same limit every minute on the channel anymore, thanks niels!

--- chanlimiter.py
+++ chanlimiter.py
@@ -5,20 +5,24 @@
 
 import weechat
 
-server_chans = 'ircnet.#cyberworld'
+count = 0 
+new_count = 0 
+server_chan = 'ircnet.#cyberworld'
 
 def cl_cmd_cb(data, signal_data):
-
-    buffer = weechat.buffer_search("irc", server_chans)
-    count = weechat.string_eval_expression("${buffer.nicklist_count}", {"buffer": buffer}, {}, {})
+    global count
+    global new_count
+    buffer = weechat.buffer_search("irc", server_chan)
+    new_count = weechat.string_eval_expression("${buffer.nicklist_count}", {"buffer": buffer}, {}, {})
     target = weechat.buffer_get_string(buffer, "name").split('.')[1]
     
-    weechat.command(buffer, '/mode ' + target + ' +l ' + count)
+    if count != new_count:
+        weechat.command(buffer, '/mode ' + target + ' +l ' + new_count)
+        count = new_count
     return weechat.WEECHAT_RC_OK
 
 weechat.register('rxtx', 'Channel limiter', '0.01', 'GPL3', 'Channel limiter', '', '')
 weechat.hook_timer(60000, 0, 0, 'cl_cmd_cb', '')
 
 # count > total users in nicklist +3 (weechat got 3 extra 'users/groups' more in the nicklist then visible)
-# todo: must work on all specified channels in var server_chans 
-#  '' : script may not change a limit when its the same after last change.
+# todo: must work on all specified channels
